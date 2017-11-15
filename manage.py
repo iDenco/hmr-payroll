8309d9b7439540971bc86a5eb6c71619a2c0dd42
iDenco <idenco90@gmail.com>
2017-11-15 17:37:43 +0700

    Initial Commit

---
+++ manage.py
@@ -0,0 +1,64 @@
+import unittest
+import coverage
+
+from flask_script import Manager
+
+from project import create_app, db
+from project.api.models import User
+
+app = create_app()
+manager = Manager(app)
+COV = coverage.coverage(
+    branch=True,
+    include='project/*',
+    omit=[
+        'project/tests/*'
+    ]
+)
+COV.start()
+
+
+@manager.command
+def recreate_db():
+    """Recreates a database."""
+    db.drop_all()
+    db.create_all()
+    db.session.commit()
+
+
+@manager.command
+def test():
+    """Runs the tests without code coverage"""
+    tests = unittest.TestLoader().discover('project/tests', pattern='test*.py')
+    result = unittest.TextTestRunner(verbosity=2).run(tests)
+    if result.wasSuccessful():
+        return 0
+    return 1
+
+
+@manager.command
+def seed_db():
+    """Seeds the database"""
+    db.session.add(User(username='micheal', email='gg@mail.com'))
+    db.session.add(User(username='test', email='test@mail.com'))
+    db.session.commit()
+
+
+@manager.command
+def cov():
+    """Runs the unit tests with coverage"""
+    tests = unittest.TestLoader().discover('project/tests', pattern='test*.py')
+    result = unittest.TextTestRunner(verbosity=2).run(tests)
+    if result.wasSuccessful():
+        COV.stop()
+        COV.save()
+        print('Coverage Summary:')
+        COV.report()
+        COV.html_report()
+        COV.erase()
+        return 0
+    return 1
+
+
+if __name__ == '__main__':
+    manager.run()
---
+++ project/db/create.sql
@@ -0,0 +1,3 @@
+CREATE DATABASE payroll_prod;
+CREATE DATABASE payroll_dev;
+CREATE DATABASE payroll_test;
